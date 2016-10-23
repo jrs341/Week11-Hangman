9cecb75403f11c5b461eb23612028e815fb00a40
Jason Sinn <jrs341@me.com>
2016-10-23 13:33:10 -0500

    the word1 file is checking the random word against the user guess

--- word1.js
+++ word1.js
@@ -1,3 +1,31 @@
 var randomWord = require('./game.js');
 
+var userGuess = process.argv[2];
+
 console.log(randomWord);
+console.log(userGuess);
+
+
+
+function CheckLetters(randomWord, userGuess) {
+	this.word = randomWord,
+	this.guess = userGuess,
+	this.guessedLetters = [];
+	this.Push = function() {
+		this.guessedLetters.push(userGuess);	
+	}
+	this.log = function() {
+		console.log(this.guessedLetters);
+	}
+	this.checkWord = function() {
+		if (randomWord.includes(userGuess)) {
+			console.log('Good Guess ' + userGuess);
+		} else {
+			console.log('Try again ' + userGuess + ' is not in the word');
+		}
+	}
+}
+
+var checkGuess = new CheckLetters(randomWord, userGuess);
+ checkGuess.checkWord();
+ // checkGuess.log();
