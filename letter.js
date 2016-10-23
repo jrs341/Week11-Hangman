ce4ab976a05ffe3743fbfbc0ca537852838f25ff
Jason Sinn <jrs341@me.com>
2016-10-23 14:32:33 -0500

    created Display constructor in letter file

--- letter.js
+++ letter.js
@@ -1,3 +1,23 @@
-var letter = require('./main1');
+var checkGuess = require('./word1');
+// var logic = require('./word1');
+// checkGuess.checkWord();
+// console.log(checkGuess);
+// console.log(checkGuess.goodGuess);
+// console.log(logic);
+// console.log(randomWord);
+// console.log(userGuess);
 
-console.log(letter.guessedLetter);
+function Display(randomWord, userGuess, logic) {
+	this.randomWord = randomWord,
+	this.guess = userGuess,
+	this.logic = logic
+	// this.log = function() {
+	// 	console.log(randomWord);
+	// 	console.log(this.guess);
+	// 	console.log(this.logic);
+	// }
+}
+
+var displayThis = new Display(checkGuess.word, checkGuess.guess, checkGuess.goodGuess);
+
+displayThis.log();
--- word1.js
+++ word1.js
@@ -7,25 +7,36 @@ console.log(userGuess);
 
 
 
-function CheckLetters(randomWord, userGuess) {
+function CheckUserGuess(randomWord, userGuess) {
 	this.word = randomWord,
 	this.guess = userGuess,
-	this.guessedLetters = [];
-	this.Push = function() {
-		this.guessedLetters.push(userGuess);	
-	}
-	this.log = function() {
-		console.log(this.guessedLetters);
-	}
+	// this.guessedLetters = [];
+	this.goodGuess = false;
+	// this.Push = function() {
+	// 	this.guessedLetters.push(userGuess);	
+	// }
+	// this.log = function() {
+	// 	console.log(this.guessedLetters);
+	// }
 	this.checkWord = function() {
 		if (randomWord.includes(userGuess)) {
 			console.log('Good Guess ' + userGuess);
+			return this.goodGuess = true;
+			// checkGuess.checkWord();
 		} else {
 			console.log('Try again ' + userGuess + ' is not in the word');
+			// checkGuess.checkWord();
 		}
 	}
 }
 
-var checkGuess = new CheckLetters(randomWord, userGuess);
- checkGuess.checkWord();
- // checkGuess.log();
+var checkGuess = new CheckUserGuess(randomWord, userGuess);
+// console.log(checkGuess.goodGuess);
+checkGuess.checkWord();
+// var logic = checkGuess.goodGuess;
+// console.log(logic);
+module.exports = checkGuess;
+// module.exports = logic;
+// module.exports = randomWord;
+// module.exports = userGuess;
+
