732f549610846e7398aa19cdeb57f1d534093edb
Jason Sinn <jrs341@me.com>
2016-12-31 11:44:51 -0600

    trying a function to display console.logs to page

--- main.js
+++ main.js
@@ -24,6 +24,18 @@ var Word = require('./word.js');
 var gameWord = require('./game.js');
 var lettersGuessed = [];
 
+(function () {
+    var old = console.log;
+    var logger = document.getElementById('log');
+    console.log = function (message) {
+        if (typeof message == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+        } else {
+            logger.innerHTML += message + '<br />';
+        }
+    }
+})();
+
 prompt.start();
 
 game = {
