3f6374f02c259358fe1e7b361f23d3b497b28583
Jason Sinn <jrs341@me.com>
2016-12-31 10:36:25 -0600

    forgot to add app.listen

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 prompt.start();
 
 game = {
